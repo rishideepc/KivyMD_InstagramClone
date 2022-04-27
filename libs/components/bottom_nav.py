64dc03e6a701b9c1c13be06b1f6bf4efc9a30bee
rishideepc <rishi1a2@gmail.com>
2022-04-27 22:45:44 +0530

    Initial Commit

---
+++ libs/components/bottom_nav.py
@@ -0,0 +1,6 @@
+from msilib.schema import Property
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.properties import StringProperty
+
+class BottomNav(MDBoxLayout):
+    avatar= StringProperty()
---
+++ libs/components/story_creator.py
@@ -0,0 +1,6 @@
+import imp
+from kivymd.uix.card import MDCard
+from kivy.properties import StringProperty
+
+class StoryCreator(MDCard):
+    avatar= StringProperty()
